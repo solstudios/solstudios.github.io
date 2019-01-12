1f562d2f34172d731485161c71eb11a59a7e5a50
Isaac Lo <isaaclo123@gmail.com>
2019-01-11 22:05:00 -0800

    search team

--- _site/js/script.js
+++ _site/js/script.js
@@ -35,3 +35,23 @@ function mail(form) {
 function goto(url) {
   window.location.href = url;
 }
+
+function filterFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('filterInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("filterUl");
+  li = ul.getElementsByTagName('filterLi');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
