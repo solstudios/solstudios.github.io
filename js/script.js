446e162a9bc0d10cfd6b149426ec87613c730bb7
Isaac Lo <isaaclo123@gmail.com>
2019-01-11 23:35:43 -0800

    good work

--- js/script.js
+++ js/script.js
@@ -36,19 +36,46 @@ function goto(url) {
   window.location.href = url;
 }
 
+function fuzzy(s, cmp) {
+  // https://stackoverflow.com/questions/9206013/javascript-fuzzy-search
+  var hay = cmp.toLowerCase(), i = 0, n = -1, l;
+  s = s.toLowerCase();
+  for (; l = s[i++] ;) if (!~(n = hay.indexOf(l, n + 1))) return false;
+  return true;
+};
+
+function filterClear() {
+  // Declare variables
+  var input, filter, ul, li, titleVal, roleVal, i, txtValue;
+
+  // clear input
+  input = document.getElementById('filterInput');
+  input.value = "";
+
+  ul = document.getElementById('filterUl');
+  li = ul.getElementsByClassName('filterLi');
+
+  // Loop through all list items, and unhide all
+  for (i = 0; i < li.length; i++) {
+    li[i].style.display = "";
+  }
+}
+
 function filterFunction() {
   // Declare variables
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, ul, li, titleVal, roleVal, i, txtValue;
   input = document.getElementById('filterInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("filterUl");
-  li = ul.getElementsByTagName('filterLi');
+  filter = input.value;
+  ul = document.getElementById('filterUl');
+  li = ul.getElementsByClassName('filterLi');
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    a = li[i].getElementsByTagName("span");
+    titleVal = a[0].textContent;
+    roleVal = a[1].textContent;
+
+    if (fuzzy(filter, titleVal) || fuzzy(filter, roleVal)) {
       li[i].style.display = "";
     } else {
       li[i].style.display = "none";
